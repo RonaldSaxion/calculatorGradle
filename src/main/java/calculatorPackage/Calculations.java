3f9ea5e07541363d04067c87bd6624f347f0e66f
Ronald Tangelder <r.j.w.t.tangelder@saxion.nl>
2020-09-14 12:21:20 +0200

    Gradle version of calculator added

---
+++ src/main/java/calculatorPackage/Calculations.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Ronald on 14-9-2020.
+ *
+ */
+package calculatorPackage;
+ 
+public class Calculations {
+    public int product(int a, int b) {
+        return a * b;
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int square(int a) {
+        return a * a;
+    }
+}
